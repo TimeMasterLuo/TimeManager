46a516b0f77fff1e283b319d40d3a2ff5eb48cb9
MikeShaw1 <1481796592@qq.com>
2020-10-06 16:42:20 +0800

    UI: a easy prototype base (5%).--xy 2020.10.06

---
+++ app/src/main/java/com/example/timemanager/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.timemanager
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button  */
+    fun sendMessage(view: View?) {
+        // Do something in response to button
+    }
+}
