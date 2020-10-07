a811e38d0fdc3c82b9c52eea1381c57d042d3224
MikeShaw1 <1481796592@qq.com>
2020-10-07 16:13:46 +0800

    UI: a easy prototype base (35%).--xy 2020.10.07

--- app/src/main/java/com/example/timemanager/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/timemanager/ui/home/HomeFragment.kt
@@ -1,14 +1,18 @@
 package com.example.timemanager.ui.home
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.timemanager.R
+import com.example.timemanager.SetAlarm
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +30,17 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val btn : Button = root.findViewById(R.id.alarm_button)
+        btn.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                textView.text = "Clicked"
+                val intent = Intent(getActivity(), SetAlarm::class.java).apply {
+                }
+                startActivity(intent)
+            }
+        })
         return root
     }
+
 }
