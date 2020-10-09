c5cb9ae7c571cc8c72791c0363a4bf0a1cfd98d9
MikeShaw1 <1481796592@qq.com>
2020-10-09 23:47:28 +0800

    UI: a easy prototype base (85%).--xy 2020.10.09

--- app/src/main/java/com/example/timemanager/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/timemanager/ui/dashboard/DashboardFragment.kt
@@ -1,14 +1,19 @@
 package com.example.timemanager.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.timemanager.R
+import com.example.timemanager.SetAlarm
+import com.example.timemanager.analyze
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class DashboardFragment : Fragment() {
@@ -28,6 +33,15 @@ class DashboardFragment : Fragment() {
         val textView: TextView = root.findViewById(R.id.text_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
+        })
+        val btn1 : FloatingActionButton = root.findViewById(R.id.analyze_button)
+        btn1.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                textView.text = "Clicked analyze button"
+                val intent = Intent(activity, analyze::class.java).apply {
+                }
+                startActivity(intent)
+            }
         })
         return root
     }
