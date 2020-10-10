e8ca765a5a4ea6754f9276c2c9c31568f79bf27c
MikeShaw1 <1481796592@qq.com>
2020-10-10 22:22:07 +0800

    debug: 修复部分机型图片显示不全、超出边界,增加了一些页面. --XY 2020.10.10

---
+++ app/src/main/java/com/example/timemanager/HistoryDetail.kt
@@ -0,0 +1,11 @@
+package com.example.timemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoryDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_detail)
+    }
+}
