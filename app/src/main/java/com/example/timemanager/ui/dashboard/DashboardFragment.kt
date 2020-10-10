164e708df49c274269e70ede96f55ace8e9e87ee
MikeShaw1 <1481796592@qq.com>
2020-10-10 22:32:57 +0800

    debug: 修复部分机型图片显示不全、超出边界,增加了一些页面. --XY 2020.10.10

--- app/src/main/java/com/example/timemanager/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/timemanager/ui/dashboard/DashboardFragment.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.timemanager.HistoryDetail
 import com.example.timemanager.R
 import com.example.timemanager.SetAlarm
 import com.example.timemanager.analyze
@@ -30,15 +31,19 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
             ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         val btn1 : FloatingActionButton = root.findViewById(R.id.analyze_button)
         btn1.setOnClickListener(object: View.OnClickListener {
             override fun onClick(v: View?) {
-                textView.text = "Clicked analyze button"
                 val intent = Intent(activity, analyze::class.java).apply {
+                }
+                startActivity(intent)
+            }
+        })
+        val btn2 : Button = root.findViewById(R.id.history_entry)
+        btn2.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(getActivity(), HistoryDetail::class.java).apply {
                 }
                 startActivity(intent)
             }
--- app/src/main/java/com/example/timemanager/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/timemanager/ui/notifications/NotificationsFragment.kt
@@ -1,14 +1,18 @@
 package com.example.timemanager.ui.notifications
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.timemanager.HistoryDetail
 import com.example.timemanager.R
+import com.example.timemanager.ui.login.Login
 
 class NotificationsFragment : Fragment() {
 
@@ -22,10 +26,7 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel =
             ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return root
     }
 }
