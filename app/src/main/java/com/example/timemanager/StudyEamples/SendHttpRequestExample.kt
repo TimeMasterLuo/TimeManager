daa7d8ac9381d58e49dae1a378312286874c7e2d
MikeShaw1 <42696208+MikeShaw1@users.noreply.github.com>
2020-11-16 21:19:31 +0800

    add sendhttpstudyexample  --by xy 2020.11.16

--- app/src/main/java/com/example/timemanager/StudyEamples/SendHttpRequestExample.kt
+++ app/src/main/java/com/example/timemanager/StudyEamples/SendHttpRequestExample.kt
@@ -3,56 +3,15 @@ package com.example.timemanager.StudyEamples
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.timemanager.R
-import android.R.attr.password
-import android.content.Context
-import android.graphics.Bitmap
-import android.os.Build
-import android.util.LruCache
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.Response
 import com.android.volley.toolbox.*
-import org.json.JSONException
+import com.example.timemanager.utils.networkRequest.MySingleton
 import org.json.JSONObject
 
-
-class MySingleton constructor(context: Context) {
-    companion object {
-        @Volatile
-        private var INSTANCE: MySingleton? = null
-        fun getInstance(context: Context) =
-            INSTANCE ?: synchronized(this) {
-                INSTANCE ?: MySingleton(context).also {
-                    INSTANCE = it
-                }
-            }
-    }
-    val imageLoader: ImageLoader by lazy {
-        ImageLoader(requestQueue,
-            object : ImageLoader.ImageCache {
-                private val cache = LruCache<String, Bitmap>(20)
-                override fun getBitmap(url: String): Bitmap {
-                    return cache.get(url)
-                }
-
-                override fun putBitmap(url: String, bitmap: Bitmap) {
-                    cache.put(url, bitmap)
-                }
-            })
-    }
-    val requestQueue: RequestQueue by lazy {
-        // applicationContext is key, it keeps you from leaking the
-        // Activity or BroadcastReceiver if someone passes one in.
-        Volley.newRequestQueue(context.applicationContext)
-    }
-    fun <T> addToRequestQueue(req: Request<T>) {
-        requestQueue.add(req)
-    }
-}
 
 class SendHttpRequestExample : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,47 +20,22 @@ class SendHttpRequestExample : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.text)
         val log = findViewById<TextView>(R.id.log)
         val requestcontent = findViewById<TextView>(R.id.request)
-        // Instantiate the cache
-        val cache = DiskBasedCache(cacheDir, 1024 * 1024) // 1MB cap
 
-        // Set up the network to use HttpURLConnection as the HTTP client.
-        val network = BasicNetwork(HurlStack())
-
-        // Instantiate the RequestQueue with the cache and network. Start the queue.
-        val requestQueue = RequestQueue(cache, network).apply {
-            start()
-        }
-
+        //定义URL连接
         val url = "https://www.baidu.com"
         val url2 = "http://59.78.38.19:8080/login"
-        val url3 = "http://apis.baidu.com/apistore/weatherservice/citylist"
-        // Formulate the request and handle the response.
-//        val stringRequest = StringRequest(Request.Method.GET, url,
-//            Response.Listener<String> { response ->
-//                // Do something with the response
-//                textView.text = "Response is: ${response.substring(0, 1500)}"
-//            },
-//            Response.ErrorListener { error ->
-//                // Handle error
-//                textView.text = "ERROR: %s".format(error.toString())
-//            })
+        //定义发送的json数据，JSONObject初始化的其他方式还需自行探索
+        val params = JSONObject("""{"username":"mike name222222", "password":"123456"}""")
 
-//        // Add the request to the RequestQueue.
-//        requestQueue.add(stringRequest)
-
-        val params = JSONObject()
-        try {
-            params.put("username", "mikeshaw")
-            params.put("password", "123456")
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-        val params2 = JSONObject("""{"username":"mike name222222", "password":"123456"}""")
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, url2, params2,
-            Response.Listener { response ->
+        //发送请求
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.POST, url2, params,
+            //成功获取返回时的callback
+            { response ->
                 textView.text = "Response: %s${response}"
             },
-            Response.ErrorListener { error ->
+            //失败情况调用的callback
+            { error ->
                 // TODO: Handle error
                 Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT)
                     .show();
@@ -110,8 +44,10 @@ class SendHttpRequestExample : AppCompatActivity() {
         )
 
         // Access the RequestQueue through your singleton class.
+        // 下面这行意思是将你的request加入到android维护的一个线程队列中，这个队列会依据其自带逻辑处理你的request
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
 
+        //requestcontent是页面的一个edittext的指针，它将显示你request的返回消息
         requestcontent.text=jsonObjectRequest.toString()
 
         // ...
@@ -121,62 +57,36 @@ class SendHttpRequestExample : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.text)
         val log = findViewById<TextView>(R.id.log)
         val requestcontent = findViewById<TextView>(R.id.request)
-        // Instantiate the cache
-        val cache = DiskBasedCache(cacheDir, 1024 * 1024) // 1MB cap
-
-        // Set up the network to use HttpURLConnection as the HTTP client.
-        val network = BasicNetwork(HurlStack())
-
-        // Instantiate the RequestQueue with the cache and network. Start the queue.
-        val requestQueue = RequestQueue(cache, network).apply {
-            start()
-        }
-
-        val url = "https://www.baidu.com"
+        //定义URL连接
         val url2 = "http://59.78.38.19:8080/login"
-        val url3 = "http://apis.baidu.com/apistore/weatherservice/citylist"
-        // Formulate the request and handle the response.
-//        val stringRequest = StringRequest(Request.Method.GET, url,
-//            Response.Listener<String> { response ->
-//                // Do something with the response
-//                textView.text = "Response is: ${response.substring(0, 1500)}"
-//            },
-//            Response.ErrorListener { error ->
-//                // Handle error
-//                textView.text = "ERROR: %s".format(error.toString())
-//            })
+        //定义发送的json数据，JSONObject初始化的其他方式还需自行探索
+        val params = JSONObject("""{"username":"mike name222222", "password":"123456"}""")
 
-//        // Add the request to the RequestQueue.
-//        requestQueue.add(stringRequest)
-
-        val params1 = JSONObject()
-        try {
-            params1.put("username", "mikeshaw")
-            params1.put("password", "123456")
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-        //Toast.makeText(getApplicationContext(), params1.toString(), Toast.LENGTH_SHORT).show();
-        val map: MutableMap<String, String> = HashMap()
-        map["username"] = "mikeshaw"
-        map["password"] = "123456"
-        val params2 = JSONObject("""{"username":"mike name", "password":"123456"}""")
-        Toast.makeText(getApplicationContext(), params2.toString(), Toast.LENGTH_SHORT).show();
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, url2, params2,
-            Response.Listener { response ->
+        //发送请求
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.POST, url2 , params,
+            //成功获取返回时的callback
+            { response ->
+                Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_SHORT)
+                    .show();
                 textView.text = "Response: %s${response}"
             },
-            Response.ErrorListener { error ->
+            //失败情况调用的callback
+            { error ->
                 // TODO: Handle error
-                //Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
-
-                log.text = "Response: %s".format(error.toString())
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT)
+                    .show();
+                log.text = "error: %s".format(error.toString())
             }
         )
 
         // Access the RequestQueue through your singleton class.
+        // 下面这行意思是将你的request加入到android维护的一个线程队列中，这个队列会依据其自带逻辑处理你的request
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
 
+        //requestcontent是页面的一个edittext的指针，它将显示你request的返回消息
         requestcontent.text=jsonObjectRequest.toString()
+
+        // ...
     }
 }
