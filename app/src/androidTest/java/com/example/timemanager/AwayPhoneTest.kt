98795237b935657ce0e2571181898585b40497ce
zmyyouyu <1440358080@qq.com>
2021-01-07 21:55:38 +0800

    feat:单元测试

--- app/src/androidTest/java/com/example/timemanager/AwayPhoneTest.kt
+++ app/src/androidTest/java/com/example/timemanager/AwayPhoneTest.kt
@@ -15,6 +15,7 @@ import com.google.android.material.tabs.TabItem
 import com.google.android.material.tabs.TabLayout
 import mehdi.sakout.fancybuttons.FancyButton
 import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -81,25 +82,61 @@ class AwayPhoneTest {
         Espresso.onView(ViewMatchers.withId(R.id.whitelist))
         .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(Button::class.java))).perform(ViewActions.click())
 
-        Espresso.onView(ViewMatchers.withId(R.id.whitelistTab))
-        .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(TabLayout::class.java))).perform(ViewActions.click())
-
-        Espresso.onView(ViewMatchers.withId(R.id.inWhitelist))
-        .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(TabItem::class.java))).perform(ViewActions.click())
-
-        Espresso.onView(ViewMatchers.withId(R.id.notinWhitelist))
-        .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(TabItem::class.java))).perform(ViewActions.click())
-
-        Espresso.onView(ViewMatchers.withId(R.id.inWitelistView))
-        .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(SwipeMenuListView::class.java))).perform(ViewActions.swipeLeft())
-
-//        Espresso.onView(ViewMatchers.withId(R.id.startAwayPhone))
-//            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(FancyButton::class.java))).perform(ViewActions.click())
+//        Espresso.onView(
+//            allOf(
+//                ViewMatchers.withId(R.id.whitelistTab),
+//                ViewMatchers.withChild(ViewMatchers.withId(R.id.inWhitelist))
+//            )
+//        ).check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(TabItem::class.java))).perform(ViewActions.click())
 //
-//        Espresso.onView(ViewMatchers.withId(R.id.stopAwayPhone))
-//            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(FancyButton::class.java))).perform(ViewActions.click())
-//
-//        Espresso.onView(ViewMatchers.withId(R.id.close))
-//            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(Button::class.java))).perform(ViewActions.click())
+        Espresso.onView(
+            allOf(
+                ViewMatchers.withText("未加入白名单")
+            )
+        ).perform(ViewActions.click())
+
+        Espresso.onView(
+            allOf(
+                ViewMatchers.withChild(ViewMatchers.withText("Chrome"))
+            )
+        ).perform(ViewActions.swipeLeft())
+
+        Espresso.onView(
+            allOf(
+                ViewMatchers.withText("已加入白名单")
+            )
+        ).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.button_backward)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.startAwayPhone))
+            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(FancyButton::class.java))).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.stopAwayPhone))
+            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(FancyButton::class.java))).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.close))
+            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(Button::class.java))).perform(ViewActions.click())
+    }
+
+    @Test
+    fun awayPhoneFocus() {
+        Espresso.onView(ViewMatchers.withId(R.id.nav_view))
+            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(BottomNavigationView::class.java)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.away_phone_button))
+            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(Button::class.java))).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.focus))
+            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(ImageView::class.java))).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.startAwayPhone))
+            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(FancyButton::class.java))).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.stopAwayPhone))
+            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(FancyButton::class.java))).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.close))
+            .check(ViewAssertions.matches(ViewMatchers.isAssignableFrom(Button::class.java))).perform(ViewActions.click())
     }
 }
